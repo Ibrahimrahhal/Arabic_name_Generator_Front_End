45edb94bb1992278a48e5649c83e960dacf5e20d
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2018-12-02 03:10:51 -0800

    added regenerate button and random method

--- src/app/names-view/names-view.component.ts
+++ src/app/names-view/names-view.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {NamesService} from '../names.service';
 import { Observable } from "rxjs/";
+import {names} from '../names.interface';
 @Component({
   selector: 'app-names-view',
   templateUrl: './names-view.component.html',
   styleUrls: ['./names-view.component.css']
 })
 export class NamesViewComponent implements OnInit {
-  namesobj = null ;
+  namesobj:names ;
   fullname:string;
-  age:any;
-  gender:string;
+  isMale:boolean;
+  age:number;
+  counter:number =0;
+  toomuchclicks:boolean= false;
   constructor(private namess:NamesService) {
     namess.getNames().subscribe((data)=>
        {
@@ -18,10 +21,8 @@ export class NamesViewComponent implements OnInit {
          this.fullname =  this.namesobj.randmalename[0]+" "+
                           this.namesobj.randmalename[1]+" "+
                           this.namesobj.randsurname[0] ;
-        // this.age = parseInt(Math.random()*80);
-        this.gender =
-        "ذكر"
-        ;
+        this.age = Math.floor(Math.random()*80);
+        this.isMale = true;
 
 
     });
@@ -33,6 +34,25 @@ export class NamesViewComponent implements OnInit {
     // this.fullname = this.namesobj.nmale[0]+this.namesobj.nmale[1]+this.namesobj.surnames[0];
 
 
+  }
+
+  random(){
+    this.age = Math.ceil(Math.random()*80);
+    if(Math.random()>=0.49) {
+      this.isMale = true  ;
+      this.fullname =  this.namesobj.randmalename[Math.floor(Math.random()*3)]+" "+
+                       this.namesobj.randmalename[Math.floor(Math.random()*3)]+" "+
+                       this.namesobj.randsurname[Math.floor(Math.random()*3)] ;
+    } else {
+      this.isMale = false  ;
+      this.fullname =  this.namesobj.randfmalename[Math.floor(Math.random()*3)]+" "+
+                       this.namesobj.randmalename[Math.floor(Math.random()*3)]+" "+
+                       this.namesobj.randsurname[Math.floor(Math.random()*3)] ;
+
+
+    }
+    this.counter++;
+    if(this.counter >=12) this.toomuchclicks =true;
   }
 
 }
